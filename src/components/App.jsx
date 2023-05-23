125dc1eeb47bac410c957e19003410ac26dc7cde
HelenGachkovska <ogachkovska@gmail.com>
2023-05-23 22:21:50 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,99 +1,62 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import ContactsForm from './ContactsForm/index';
 import ContactList from './ContactsList/index';
 import ContactFilter from './ContactsFilter/index';
+import { AppContainer, AppTitle } from './styled';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const startContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+function App() {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts')) ?? startContacts;
+  });
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevPros, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(
+    () => localStorage.setItem('contacts', JSON.stringify(contacts)),
+    [contacts]
+  );
 
-  hanlerSubmitForm = props => {
-    const { name, number } = props;
-    const arrayValue = this.state.contacts.map(el => el.name.toLowerCase());
+  const createContact = props => {
+    const { name, number, id = nanoid() } = props;
 
+    const arrayValue = contacts.map(el => el.name.toLowerCase());
+
+    console.log(arrayValue);
     if (arrayValue.includes(name.toLowerCase())) {
       alert(`${name} is already in contacts`);
       return;
     }
-    this.setState(prevState => ({
-      contacts: prevState.contacts.concat({
-        id: nanoid(),
-        name,
-        number,
-      }),
-    }));
+
+    setContacts([...contacts, { name, number, id }]);
   };
 
-  handlerFilterInput = e => {
-    this.setState({ filter: e.target.value });
+  const handlerFilterInput = e => {
+    setFilter(e.target.value);
   };
 
-  handlerDeleteItem = id => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(contact => contact.id !== id),
-    }));
+  const handlerDeleteItem = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
 
-  render() {
-    const filteredOutArray = this.state.contacts.filter(el =>
-      el.name.toLowerCase().includes(this.state.filter.toLowerCase())
-    );
+  const filteredOutArray = contacts.filter(el =>
+    el.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-    return (
-      <div
-        style={{
-          // height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <ContactsForm onSubmit={this.hanlerSubmitForm} />
-        <h2
-          style={{
-            fontStyle: 'normal',
-            fontWeight: 600,
-            fontSize: 42,
-            lineHeight: 1.62,
-            textAlign: 'center',
-          }}
-        >
-          Contacts
-        </h2>
-        <ContactFilter
-          value={this.state.filter}
-          onChange={this.handlerFilterInput}
-        />
-        <ContactList
-          contacts={filteredOutArray}
-          onDelete={this.handlerDeleteItem}
-        />
-      </div>
-    );
-  }
+  return (
+    <AppContainer>
+      <ContactsForm createContact={createContact} />
+      <AppTitle>Contacts</AppTitle>
+      <ContactFilter value={filter} onChange={handlerFilterInput} />
+      <ContactList contacts={filteredOutArray} onDelete={handlerDeleteItem} />
+    </AppContainer>
+  );
 }
+
+export default App;
